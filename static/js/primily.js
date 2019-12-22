da62025bdeae61072637aec7e2aacb9cc954c142
balu <balu.doddi@byteorigin.com>
2019-12-22 13:40:51 +0530

    js modified

--- static/js/primily.js
+++ static/js/primily.js
@@ -2,10 +2,12 @@ $(document).ready(function () {
     $('.slider').slider({full_width: true})
 });
 
+$(document).ready(function(){
 $('.carousel.carousel-slider').carousel({
     fullWidth: true,
     indicators: true
   });
+});
 
   $(document).ready(function(){
     $('.collapsible').collapsible();
