f16a345cd1e6170bbb39a5ea507dd18af0026660
Wayne Clingingsmith <wayne@clingingsmith.com>
2020-01-08 00:41:50 -0800

    Carousel SlideView auto-play

--- src/components/SlideView/index.js
+++ src/components/SlideView/index.js
@@ -19,7 +19,10 @@ const settings = {
     infinite: true,
     speed: 500,
     slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 10000,
+    autoplaySpeed: 5000
 };
 
 export default class SlideView extends React.Component {
