512a1782bc336085ce8dc09de10b57b592fdf6da
Wayne Clingingsmith <wayne@clingingsmith.com>
2020-01-07 22:42:06 -0800

    Changed to Slick Carousel

--- src/templates/home-page.js
+++ src/templates/home-page.js
@@ -1,36 +1,45 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link, graphql, StaticQuery } from 'gatsby'
+import {Link, graphql, StaticQuery} from 'gatsby'
 import CarouselComponent from '../components/carousel'
+import SlideView from "../components/SlideView";
 
 export class Home extends React.Component {
-  render() {
-    
-    const { home } = this.props.data
+    render() {
 
-    return (
-    <div className="container">
-     <CarouselComponent content={home.sliderImages} />
-     <div className="carousel carousel-slider center">
-  {home.sliderImages.map((image, index) => (
-  <div className="carousel-item blue white-text" href="#one!" key={index}>
-    <img src={image.imagesrc} className="responsive-img" alt={image.imagealt}/>
-  </div>
-  ))}
-  </div>
-     <div className="row"><div className="col s12"><br></br></div></div>
-     <div className="row"><div className="col s12"><br></br></div></div>
-    </div>
-    )
-  }
-  }
-  
-  Home.propTypes = {
-    data: PropTypes.object
+        const {home} = this.props.data
+
+        return (
+
+            <div className="container">
+
+                <SlideView/>
+
+                {/*<CarouselComponent content={home.sliderImages}/>
+                <div className="carousel carousel-slider center">
+                    {home.sliderImages.map((image, index) => (
+                        <div className="carousel-item blue white-text" href="#one!" key={index}>
+                            <img src={image.imagesrc} className="responsive-img" alt={image.imagealt}/>
+                        </div>
+                    ))}
+                </div>
+                <div className="row">
+                    <div className="col s12"><br></br></div>
+                </div>
+                <div className="row">
+                    <div className="col s12"><br></br></div>
+                </div>*/}
+            </div>
+        )
     }
-  
-  export default () => (
-      <StaticQuery
+}
+
+Home.propTypes = {
+    data: PropTypes.object
+}
+
+export default () => (
+    <StaticQuery
         query={graphql`
           query HomeQuery {
             allMarkdownRemark(
@@ -51,8 +60,7 @@ export class Home extends React.Component {
             }
           }
         `}
-        render={(data, count) => <Home data={data.allMarkdownRemark.edges[0].node.frontmatter} count={count} />}
-      />
-    )
-  
-  
+        render={(data, count) => <Home data={data.allMarkdownRemark.edges[0].node.frontmatter} count={count}/>}
+    />
+)
+
