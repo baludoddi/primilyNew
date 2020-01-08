cbcdc1ad0822d6894fca48c288624755a488c121
Wayne Clingingsmith <wayne@clingingsmith.com>
2020-01-07 22:59:51 -0800

    Change to Slick Carousel

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -14,6 +14,11 @@ const TemplateWrapper = ({ footerData = null, navbarData = null, children }) => 
       <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
       <script src={withPrefix('js/primily.js')} type="text/javascript" />
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
+
+      {/* for Slick Carousel */}
+        <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+
     </Helmet>
     <Navbar data={navbarData} />
     <main>{children}</main>
