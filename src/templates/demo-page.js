78b5d93da90c27d967db2167cd9991096b4de5a4
Wayne Clingingsmith <wayne@clingingsmith.com>
2020-01-17 13:12:41 -0800

    Change Demo section carousel to same the SlideView component

--- src/templates/demo-page.js
+++ src/templates/demo-page.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from 'prop-types'
 //import { Link, graphql, StaticQuery } from 'gatsby'
 import { graphql, StaticQuery } from 'gatsby'
+import SlideView from "../components/SlideView";
 
 export class Demo extends React.Component {
     render() {
@@ -9,38 +10,9 @@ export class Demo extends React.Component {
       const { demo } = this.props.data
 
       return (
-        <div>
-            <div className="row"><div className="col s12"><br></br></div></div>
-            <div className="row"><div className="col s12"><br></br></div></div>
-            <div className="container">
-                <div className="row">
-                    <div className="col s12 feature-items center-align">
-                        <h5>{demo.demoHead}</h5>
-                        <p>{demo.description}</p>
-                    </div>
-                    <div className="col s3"></div>
-                    <div className="col s3"><img src={demo.demoImages.src1} alt="" /></div>
-                    <div className="col s0"></div>
-                    <div className="col s3"><img src={demo.demoImages.src2} alt="" /></div>
-                    <div className="col s3"></div>
-                </div>
-                <br></br>
-                {/* <div className="row">
-                    <hr />
-                    <h5>Contact Us</h5>
-                    <form
-                        name="contact-form"
-                        method="post"
-                        data-netlify="true"
-                        data-netlify-honeypot="bot-field" >
-                        <input name="name" placeholder="Your name" type="text" />
-                        <input name="email" placeholder="Your email address" type="email" />
-                        <textarea name="message" placeholder="Your message" id="" cols="30" rows="40" />
-                        <button className="waves-effect waves-light btn">Send</button>
-                    </form>
-                </div> */}
-            </div>
-        </div>
+          <div className="container">
+              <SlideView content={demo.sliderImages} />
+          </div>
     )
 }
 }
@@ -52,27 +24,26 @@ Demo.propTypes = {
 export default () => (
     <StaticQuery
       query={graphql`
-        query DemoQuery {
-          allMarkdownRemark(
-            filter: { frontmatter: { templateKey: { eq: "demo-page" } } }
-          ) {
-            edges {
-              node {
-                frontmatter {
+          query DemoQuery {
+            allMarkdownRemark(
+              filter: { frontmatter: { templateKey: { eq: "demo-page" } } }
+            ) {
+              edges {
+                node {
+                  frontmatter {
                     demo {
-                        demoHead
-                        description
-                        demoImages {
-                          src1
-                          src2
-                        }
+                      sliderImages {
+                        imageSrc
+                        imagealt
+                        imageText
+                      }
                     }
+                  }
                 }
               }
             }
           }
-        }
-      `}
+        `}
       render={(data, count) => <Demo data={data.allMarkdownRemark.edges[0].node.frontmatter} count={count} />}
     />
   )
